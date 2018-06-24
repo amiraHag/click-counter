4412646af28b84e9d63f0d7cd50324b5634dc7a4
amira <amira071846@feng.bu.edu.eg>
2018-06-24 16:44:27 +0200

    Initial Commit

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -13,6 +13,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // ********** Label ***********
+        //Allocate and Initialize the label
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
+        label.text = "0"
+        
+        //Add the label to the view hierarchy
+        view.addSubview(label)
+        
+        // ********** Button ***********
+        //Allocate and Initialize the Button
+        let button = UIButton()
+        button.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        
+        //Add Button to view hierarchy
+        view.addSubview(button)
+        
     }
 
     override func didReceiveMemoryWarning() {
